19ad52bb63c95866c627ff965ad6f1cf2c659d5e
Paul Koehler <paul.koehler@gmail.com>
2023-07-11 17:11:48 -0700

    update dist path

--- server/server.js
+++ server/server.js
@@ -3,10 +3,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static("../../../client/dist"));
+app.use(express.static("../client/dist"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+
